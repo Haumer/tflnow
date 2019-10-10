602a59e3667033dc1b0997d1f757db56b64fe6b5
alexander <alexander.haumer@me.com>
2019-10-10 09:46:49 +0100

    changed the incidents show page such that the stations in question have a red border if there is a fault

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,7 +1,21 @@
 class IncidentsController < ApplicationController
   def show
     @incident = Incident.find(params[:id])
-    @incident.line = Line.find(params[:line_id])
     @line = Line.find(params[:line_id])
+    @stations = @line.stations
+
+    @stations.select do |station|
+      @incident.reason.downcase.include?(station.station_name.downcase)
+    end.each do |sstation|
+      sstation.update!(station_status: "Fault")
+    end
+
+    @stations.reject do |station|
+      @incident.reason.downcase.include?(station.station_name.downcase)
+    end.each do |sstation|
+      sstation.update!(station_status: "Good Service")
+    end
+
+    @stations = @stations.sort_by { |e| e.station_number }
   end
 end
--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,5 +2,6 @@ class LinesController < ApplicationController
   def show
     @line = Line.find(params[:id])
     @incidents = @line.incidents
+    @stations = @line.stations
   end
 end
