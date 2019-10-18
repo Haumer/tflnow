94e04d9cd7fe7fb585b7a5c2d0af3b190ff4bac0
alexander <alexander.haumer@me.com>
2019-10-18 12:20:38 +0100

    live status indicator for navbar

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,5 +1,18 @@
+require "open-uri"
+
 class Line < ApplicationRecord
   has_many :incidents, dependent: :destroy
   has_many :station_lines
   has_many :stations, through: :station_lines
+
+  def self.status_api
+    url = "https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status"
+    req = open(url)
+    response = req.read
+    json = JSON.parse(response)
+    json.reject do |line|
+      p line
+      line['lineStatuses'].first['statusSeverityDescription'] == 'Good Service'
+    end.empty?
+  end
 end
