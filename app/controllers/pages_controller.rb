2fb83c38373e4bdfa4fdf62bebf1c806fe988a91
alexander <alexander.haumer@me.com>
2020-03-15 01:51:33 +0000

    commented api call since automated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    CheckTflJob.perform_now
+    # CheckTflJob.perform_now # uncomment if no sidekiq or redis active
     @good_lines = Line.where(status: "Good Service").order("LENGTH(name) ASC") + Line.where(status: "Service Closed").order("LENGTH(name) ASC")
     @bad_lines = Line.where.not(status: "Good Service").order("LENGTH(name) ASC") - Line.where(status: "Service Closed").order("LENGTH(name) ASC")
     @incidents = Incident.all
