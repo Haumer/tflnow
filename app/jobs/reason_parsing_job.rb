e6465f3af63b41b0f5e5db02bc7e4aca24cfb686
alexander <alexander.haumer@me.com>
2020-03-18 20:19:09 +0000

    added dependet destroy for incident reason and improved the job such that it only creates a new reason when the message changes

--- app/jobs/reason_parsing_job.rb
+++ app/jobs/reason_parsing_job.rb
@@ -3,14 +3,30 @@ class ReasonParsingJob < ApplicationJob
 
   def perform(*args)
     puts "Starting: <================================================="
-    Incident.all.each do |incident|
+    Incident.all.find_each do |incident|
       # NOTE: doesnt work for Hammersmith and Waterterloo => '&' in name (maybe highbury and islington too?)
       if incident.reason.downcase.split(":").first.include?(incident.line.name.downcase)
-        reason = Reason.create!(content: incident.reason.split(":").last.strip)
-        p IncidentReason.create(reason: reason, incident: incident)
+        reason = nil
+        # Check is the Reason already exists
+        if Reason.find_by_content(incident.reason.split(":").last.strip).nil?
+          reason = Reason.create!(content: incident.reason.split(":").last.strip)
+          puts "NEW #{reason.content}"
+        else
+          puts ">> #{incident.reason.strip} <<already exists!"
+          reason = Reason.find_by_content(incident.reason.strip)
+        end
+        IncidentReason.create(reason: reason, incident: incident)
       else
-        reason = Reason.create!(content: incident.reason)
-        p IncidentReason.create(reason: reason, incident: incident)
+        reason = nil
+        # Check is the Reason already exists
+        if Reason.find_by_content(incident.reason.strip).nil?
+          reason = Reason.create!(content: incident.reason.strip)
+          puts "NEW #{reason.content}"
+        else
+          puts ">> #{incident.reason.strip} <<already exists!"
+          reason = Reason.find_by_content(incident.reason.strip)
+        end
+        IncidentReason.create(reason: reason, incident: incident)
       end
     end
     puts "=======================================================> End"
--- app/models/reason.rb
+++ app/models/reason.rb
@@ -1,4 +1,4 @@
 class Reason < ApplicationRecord
-  has_many :incident_reasons
+  has_many :incident_reasons, dependent: :destroy
   has_many :incidents, through: :incident_reasons
 end
