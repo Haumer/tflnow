7ee8d6694a81c7ba3650cccf1d8051f94e141ee8
alexander <alexander.haumer@me.com>
2020-03-22 02:55:28 +0000

    day_log

--- db/migrate/20200322024046_change_ledger_columns.rb
+++ db/migrate/20200322024046_change_ledger_columns.rb
@@ -2,7 +2,7 @@ class ChangeLedgerColumns < ActiveRecord::Migration[6.0]
   def up
     rename_column :ledgers, :api_log, :old_api_log
     rename_column :ledgers, :uptime_log, :old_uptime_log
-    rename_column :ledgers, :day_log, :old_day_log
+    # rename_column :ledgers, :day_log, :old_day_log
     add_column :ledgers, :api_log, :datetime, array: true, default: [DateTime.now]
     add_column :ledgers, :uptime_log, :datetime, array: true, default: [DateTime.now]
     add_column :ledgers, :day_log, :text, array: true, default: []
