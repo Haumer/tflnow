2d8a6e14f551e2adb44d59c5bfa69884d7de8c6b
alexander <alexander.haumer@me.com>
2020-03-24 22:09:30 +0000

    further refactor and correction of api call.

--- app/jobs/check_tfl_job.rb
+++ app/jobs/check_tfl_job.rb
@@ -10,9 +10,10 @@ class CheckTflJob < ApplicationJob
     json = JSON.parse(response)
     json.each do |line|
       if Line.find_by_name(line["name"]).status != line["lineStatuses"].first["statusSeverityDescription"]
-        line = Line.find_by_name(line["name"]).update(status: line["lineStatuses"].first["statusSeverityDescription"], last_update: line["lineStatuses"].first["created"])
-        if line.last_update > Time.now - 10.minutes
-          Incident.create(line: Line.where(name: line["name"]).first, reason: line['lineStatuses'].first['reason'], status: line["lineStatuses"].first["statusSeverityDescription"])
+        found_line = Line.find_by_name(line["name"])
+        found_line.update(status: line["lineStatuses"].first["statusSeverityDescription"], last_update: line["lineStatuses"].first["created"])
+        if found_line.last_update > Time.now - 10.minutes
+          Incident.create(line: found_line, reason: line['lineStatuses'].first['reason'], status: line["lineStatuses"].first["statusSeverityDescription"])
         end
       end
     end
