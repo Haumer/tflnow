1999ce1ce3d6ffbb50e0d1b31a36d75dfca801d6
alexander <alexander.haumer@me.com>
2020-03-24 22:16:34 +0000

    simplifying reason parsing

--- app/jobs/reason_parsing_job.rb
+++ app/jobs/reason_parsing_job.rb
@@ -3,36 +3,25 @@ class ReasonParsingJob < ApplicationJob
 
   def perform(*args)
     puts "Starting: <================================================="
-    incidents = Incident.where(created_at: 2.days.ago..DateTime::Infinity.new )
+    # find all incidents which are less than 2 days old
+    incidents = Incident.where(created_at: 2.days.ago..DateTime::Infinity.new)
     incidents.find_each do |incident|
       # NOTE: doesnt work for Hammersmith and Waterterloo => '&' in name (maybe highbury and islington too?)
-      if incident.reason.downcase.split(":").first.include?(incident.line.name.downcase)
-        reason = nil
-        # Check is the Reason already exists
-        if Reason.find_by_content(incident.reason.split(":").last.strip).nil?
-          reason = Reason.create!(content: incident.reason.split(":").last.strip)
-          severity = incident.reason.match(/severe\s?(\w+)/i)[0] unless incident.reason.match(/severe\s?(\w+)/i).nil?
-          severity = incident.reason.match(/minor\s?(\w+)/i)[0] unless incident.reason.match(/severe\s?(\w+)/i).nil?
-          reason.update(severity: severity)
-          puts "NEW #{reason.content}"
-        else
-          puts ">> #{incident.reason.strip} <<already exists!"
-          reason = Reason.find_by_content(incident.reason.strip)
-        end
+      reason = nil
+      # Check is the Reason already exists
+      if Reason.find_by_content(incident.reason.strip).nil?
+        reason = Reason.create!(content: incident.reason.strip)
+        severity = incident.reason.match(/severe\s?(\w+)/i)[0] unless incident.reason.match(/severe\s?(\w+)/i).nil?
+        severity = incident.reason.match(/minor\s?(\w+)/i)[0] unless incident.reason.match(/severe\s?(\w+)/i).nil?
+        reason.update(severity: severity)
+        puts "NEW #{reason.content}"
+      else
+        puts ">> #{incident.reason.strip} << already exists!"
+        reason = Reason.find_by_content(incident.reason.strip)
+      end
+      if reason.incident_reasons.any?
         IncidentReason.create(reason: reason, incident: incident)
       else
-        reason = nil
-        # Check is the Reason already exists
-        if Reason.find_by_content(incident.reason.strip).nil?
-          reason = Reason.create!(content: incident.reason.strip)
-          severity = incident.reason.match(/severe\s?(\w+)/i)[0] unless incident.reason.match(/severe\s?(\w+)/i).nil?
-          severity = incident.reason.match(/minor\s?(\w+)/i)[0] unless incident.reason.match(/severe\s?(\w+)/i).nil?
-          reason.update(severity: severity)
-          puts "NEW #{reason.content}"
-        else
-          puts ">> #{incident.reason.strip} <<already exists!"
-          reason = Reason.find_by_content(incident.reason.strip)
-        end
         IncidentReason.create(reason: reason, incident: incident)
       end
     end
