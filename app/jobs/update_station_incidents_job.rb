3f2cf11b98a598583472197c828b37f9f8551a93
alexander <alexander.haumer@me.com>
2020-06-21 22:22:34 +0100

    further refactor for UpdateStationIncidentsJob

--- app/jobs/update_station_incidents_job.rb
+++ app/jobs/update_station_incidents_job.rb
@@ -2,7 +2,7 @@ class UpdateStationIncidentsJob < ApplicationJob
   queue_as :default
 
   def perform
-    incidents = Incident.where(created_at: 2.days.ago..DateTime::Infinity.new)
+    incidents = incidents_less_than_2_days_ago
     incidents.each do |incident|
       incident.line.stations.each do |station|
         if incident_inlcudes_station_name?(station, incident)
@@ -13,6 +13,10 @@ class UpdateStationIncidentsJob < ApplicationJob
         end
       end
     end
+  end
+
+  def incidents_less_than_2_days_ago
+    Incident.where(created_at: 2.days.ago..DateTime::Infinity.new)
   end
 
   def incident_inlcudes_station_name?(station, incident)
