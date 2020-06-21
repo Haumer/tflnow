af91d31c33f9907ac0627f0def0bf1dba12cd5e8
alexander <alexander.haumer@me.com>
2020-06-21 21:48:52 +0100

    fixed incident checking

--- app/jobs/check_tfl_job.rb
+++ app/jobs/check_tfl_job.rb
@@ -11,13 +11,16 @@ class CheckTflJob < ApplicationJob
     json.each do |line|
       if status_changed_or_new_day?(line)
         found_line = Line.find_by_name(line["name"])
+        puts "checking #{Line.find_by_name(line["name"]).name}"
         found_line.update(status: line["lineStatuses"].first["statusSeverityDescription"], last_update: line["lineStatuses"].first["created"])
-        if found_line.last_update > DateTime.now - 10.minutes
+        if changed_10_mins_ago?(line)
           Incident.create(line: found_line, reason: line['lineStatuses'].first['reason'], status: line["lineStatuses"].first["statusSeverityDescription"])
         end
       end
     end
   end
+
+  private
 
   def status_changed_or_new_day?(line)
     return if line.nil? || line.empty?
@@ -25,5 +28,12 @@ class CheckTflJob < ApplicationJob
     check_line = Line.find_by_name(line["name"])
     status = line["lineStatuses"].first["statusSeverityDescription"]
     check_line.status != status || check_line.last_update < DateTime.now - 1.day
+  end
+
+  def changed_10_mins_ago?(line)
+    return if line.nil? || line.empty?
+
+    check_line = Line.find_by_name(line["name"])
+    check_line.last_update < DateTime.now - 10.minutes
   end
 end
