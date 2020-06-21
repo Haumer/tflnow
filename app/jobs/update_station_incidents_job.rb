a704cb67f678eb41764662bc27a86e2f3e860612
alexander <alexander.haumer@me.com>
2020-06-21 22:21:23 +0100

    refactor UpdateStationIncidentsJob

--- app/jobs/update_station_incidents_job.rb
+++ app/jobs/update_station_incidents_job.rb
@@ -4,9 +4,8 @@ class UpdateStationIncidentsJob < ApplicationJob
   def perform
     incidents = Incident.where(created_at: 2.days.ago..DateTime::Infinity.new)
     incidents.each do |incident|
-      line = incident.line
-      found = line.stations.each do |station|
-        if incident.reason.split(/:/).last.include?(station.name)
+      incident.line.stations.each do |station|
+        if incident_inlcudes_station_name?(station, incident)
           StationIncident.create(
             station: station,
             incident: incident
@@ -14,5 +13,9 @@ class UpdateStationIncidentsJob < ApplicationJob
         end
       end
     end
+  end
+
+  def incident_inlcudes_station_name?(station, incident)
+    incident.reason.split(/:/).last.include?(station.name)
   end
 end
